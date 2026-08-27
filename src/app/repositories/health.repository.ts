06a783f93f12a170c49b5e284c9a36c8d46e2fc9
Nawaz <rifaqatnawaz25@gmail.com>
2026-08-27 16:50:10 +0530

    chore: placeholder is true not false

--- src/app/repositories/health.repository.ts
+++ src/app/repositories/health.repository.ts
@@ -2,7 +2,7 @@ import { db } from "@/db";
 
 export const healthCheckRepository = async () => {
   const isDatabaseConnected = await db.execute(`SELECT 1`).then(() => true).catch(() => false);
-    const isLlmConnected = false; //TODO: Placeholder for future LLM connection check
+    const isLlmConnected = true; //TODO: Placeholder for future LLM connection check
     return new Map([
       ["Database", isDatabaseConnected],
       ["LLM", isLlmConnected]
