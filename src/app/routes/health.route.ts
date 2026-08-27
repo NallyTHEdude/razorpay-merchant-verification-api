f472d7ae89deb6fadb1cd449c7b7fa9f420f6289
Nawaz <rifaqatnawaz25@gmail.com>
2026-08-27 18:51:34 +0530

    docs: updated swagger docs for health route

--- src/app/routes/health.route.ts
+++ src/app/routes/health.route.ts
@@ -2,7 +2,23 @@ import Router from "express";
 import { healthCheck } from "../controllers/health.controller.js";
 
 const router = Router();
-
+/**
+ * @swagger
+ * /api/health:
+ * 
+ *   get:
+ *      tags:
+ *          - HealthCheck Endpoint
+ * 
+ *      summary: Health check endpoint
+ *      description: Returns the health status of the API by checking availability of all external services.
+ * 
+ *      responses:
+ *          200:
+ *            description: Health status of the API
+ *          503:
+ *            description: Service Unavailable - One or more external services are not healthy
+ */
 router.get("/", healthCheck);
 
 export default router;
