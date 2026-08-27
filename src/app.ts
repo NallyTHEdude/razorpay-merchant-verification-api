5226c4f359d73e517fef9fbda20d3b47f08f728c
Nawaz <rifaqatnawaz25@gmail.com>
2026-08-27 18:30:06 +0530

    chore: setup swagger for documentation

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,18 @@
 import express from "express";
 import { globalErrorHandler } from "@/utils/errors/globalErrorHandler";
 
+//import routes
+import swaggerRoute from "@/config/docs/swagger";
+import healthRoute from "@/app/routes/health.route";
+
 const app = express();
 
 app.use(express.json());
 
-//import routes
-import healthRoute from "@/app/routes/health.route";
+
 
 // setup routes
+app.use("/api-docs", swaggerRoute);
 app.use("/health", healthRoute);
 
 
---
+++ src/config/docs/swagger.ts
@@ -0,0 +1,57 @@
+import Router from "express";
+import swaggerJsdoc from "swagger-jsdoc";
+import { config } from "@/config";
+// @ts-expect-error swagger-model-validator does not provide TypeScript declarations by default, hence ignore
+import swaggerModelValidator from "swagger-model-validator";
+import swaggerUi from "swagger-ui-express";
+import { Request, Response } from "express";
+
+const router = Router();
+
+const options: swaggerJsdoc.Options = {
+    swaggerDefinition: {
+        openapi: "3.0.0",
+        info: {
+            title: "Merchant Analyzer API",
+            version: "1.0.0",
+            description: "API documentation for the Merchant Analyzer project.",
+        },
+        tags: [
+            {
+                name: "HealthCheck Endpoint",
+                description: "Endpoint related to health check of the API."
+            },
+            {
+                name: "Merchant Endpoints",
+                description: "Endpoints related to merchant management."
+            }
+        ],
+        servers: [
+            {
+                url: `http://localhost:${config.PORT}`,
+                description: "Local development server"
+            }
+        ],
+        components: {
+            securitySchemes: {}
+        }
+    },
+    apis: ["@/app/routes/*.ts"], // Path to the API docs
+}
+
+const swaggerSpec = swaggerJsdoc(options);
+
+
+// validate swagger spec against the OpenAPI 3.0 specification
+swaggerModelValidator(swaggerSpec);
+
+
+// Serve swagger endpoints
+router.get("/json", (_req: Request, res: Response) => {
+    res.setHeader("Content-Type", "application/json");
+    res.send(swaggerSpec);
+});
+
+router.use("/", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+export default router;
