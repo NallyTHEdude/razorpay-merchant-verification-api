6c6ee219bf2bf0508b605ce5e912ae044f9d7012
Nawaz <rifaqatnawaz25@gmail.com>
2026-08-28 09:47:46 +0530

    feat: setup merchant repository

--- src/app/repositories/merchant.repository.ts
+++ src/app/repositories/merchant.repository.ts
@@ -1,1 +1,57 @@
-// TODO: implement merchant repository methods tomorrow
+import {
+  CreateMerchantDto,
+  Merchant,
+  UpdateMerchantDto,
+} from "@/data/types/Merchant";
+import { db } from "@/db";
+import { merchants as merchantTable } from "@/db/schemas/merchants.schema";
+import { ApiError } from "@/utils/errors/ApiError";
+import { eq } from "drizzle-orm"; 
+import { StatusCodes } from "http-status-codes/build/cjs/status-codes";
+
+export const getAllMerchants = async (): Promise<Merchant[]> => {
+    return db.select().from(merchantTable);
+};
+
+export const getMerchantById = async (id: string): Promise<Merchant | null> => {
+    const [merchant] = await db.select().from(merchantTable)
+    .where(eq(merchantTable.id, id))
+    .limit(1);
+    return merchant ?? null;
+};
+
+export const getMerchantByGstNumber = async (gstNumber: string): Promise<Merchant | null> => {
+  const [merchant] = await db
+    .select()
+    .from(merchantTable)
+    .where(eq(merchantTable.gstNumber, gstNumber))
+    .limit(1);
+
+  return merchant ?? null;
+};
+
+export const createMerchant = async (merchantData: CreateMerchantDto): Promise<Merchant | null> => {
+  const [newMerchant] = await db
+    .insert(merchantTable)
+    .values(merchantData)
+    .returning();
+
+  return newMerchant ?? null;
+};
+
+export const updateMerchant = async (id: string, merchantData: UpdateMerchantDto): Promise<Merchant | null> => {
+    const [updatedMerchant] = await db.update(merchantTable)
+    .set(merchantData)
+    .where(eq(merchantTable.id, id))
+    .returning();
+
+    return updatedMerchant ?? null;
+};
+
+export const deleteMerchantById = async (id: string): Promise<Merchant | null> => {
+    const [deletedMerchant] = await db.delete(merchantTable)
+    .where(eq(merchantTable.id, id))
+    .returning();
+    
+    return deletedMerchant ?? null;
+};
