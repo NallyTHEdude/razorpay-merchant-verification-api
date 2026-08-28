0d36f4d62c8f57833b84bfd21a571f9b95ab3f41
Nawaz <rifaqatnawaz25@gmail.com>
2026-08-28 16:14:56 +0530

    feat: added validation to routes

--- src/app/routes/verification.route.ts
+++ src/app/routes/verification.route.ts
@@ -4,10 +4,16 @@ import {
     getVerificationById,
     getAllVerifications,
 } from "@/app/controllers/verification.controller";
+import { validate } from "@/app/middlewares/validate.middleware";
+import {
+    createVerificationValidator,
+    getVerificationByIdValidator,
+} from "@/app/validators/verification.validator";
+
 const router = Router();
 
 router.get("/:merchantId", getAllVerifications);
-router.get("/:merchantId/:verificationId", getVerificationById);
-router.post("/:merchantId", createVerification);
+router.get("/:merchantId/:verificationId", validate(getVerificationByIdValidator), getVerificationById);
+router.post("/:merchantId", validate(createVerificationValidator), createVerification);
 
 export default router;
