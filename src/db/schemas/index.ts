8ffb7b458bf514ccbb9be337e708246f048ba141
Nawaz <rifaqatnawaz25@gmail.com>
2026-08-28 17:05:16 +0530

    chore: created pipeline files

--- src/db/schemas/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./merchants.schema";
-export * from "./verifications.schema";
-export * from "./payments.schema";
-export * from "./rag.schema";
-export * from "./investigations.schema";
-export * from "./relations/index";
