d700e96f9808749a21f27e7106f1f1b82754c704
Nawaz <rifaqatnawaz25@gmail.com>
2026-08-28 16:01:48 +0530

    feat: added verification types

---
+++ src/data/types/Verification.d.ts
@@ -0,0 +1,19 @@
+import type { InferInsertModel, InferSelectModel } from "drizzle-orm";
+// import type { VerificationStatus, RiskLevel } from "@/data/enums/db.enums";
+import type { verifications } from "@/db/schemas/verifications.schema";
+
+export type Verification = InferSelectModel<typeof verifications>;
+
+export type NewVerification = InferInsertModel<typeof verifications>;
+
+export type VerificationIdParam = {
+  verificationId: string;
+};
+
+export type VerificationMerchantIdParam = {
+  merchantId: string;
+};
+
+export interface CreateVerificationDto {
+  merchantId: string;
+}
