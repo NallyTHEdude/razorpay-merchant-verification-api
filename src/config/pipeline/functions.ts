af6b4b2befc1cd55b6d518d9230236e5895d77b7
Nawaz <rifaqatnawaz25@gmail.com>
2026-08-30 00:12:30 +0530

    feat: inngest pipeline complete

--- src/config/pipeline/functions.ts
+++ src/config/pipeline/functions.ts
@@ -1,8 +1,13 @@
 import { inngestClient } from "./client";
+
 import { verifyPhoneNumber } from "@/app/verification-pipeline/stages/phone-number/phoneNumber.verification";
 import { gstNumberVerification } from "@/app/verification-pipeline/stages/gst-number/gstNumber.verification";
 import { logRegPrediction } from "@/app/verification-pipeline/stages/ml-prediction/logisticRegression";
 import { fetchWebstiteData } from "@/app/verification-pipeline/stages/website/website.verification";
+import { combineResults } from "@/app/verification-pipeline/stages/combine-results/combine-results";
+import { updateVerification } from "@/app/verification-pipeline/stages/update-verification/update-verification";
+
+import { PipelineResults } from "@/data/types/pipelineTypes";
 
 export const merchantPipeline = inngestClient.createFunction(
   {
@@ -14,7 +19,6 @@ export const merchantPipeline = inngestClient.createFunction(
     ],
   },
 
-
   async ({ event, step }) => {
     const { merchant, verification, recentPayments } = event.data;
 
@@ -24,37 +28,75 @@ export const merchantPipeline = inngestClient.createFunction(
       );
     });
 
-    const isPhoneNumberVerified = await step.run("verify-phone-number", async () => {
-      console.log(`Verifying phone number for merchant ${merchant.id}`);
-      return await verifyPhoneNumber(merchant.phoneNumber);
-    });
+    const isPhoneNumberVerified = await step.run(
+      "verify-phone-number",
+      async () => {
+        console.log(`Verifying phone number for merchant ${merchant.id}`);
 
-    const isGstNumberVerified = await step.run("verify-gst-number", async () => {
-      console.log(`Verifying GST for merchant ${merchant.id}`);
-      return await gstNumberVerification(merchant.gstNumber);
-    });
+        return verifyPhoneNumber(merchant.phoneNumber);
+      },
+    );
 
-    // TODO: Implement the actual website verification logic
-    const {websiteData, isWebsiteVerified} = await step.run("verify-website", async () => {
-      console.log(`Verifying website and getting data for merchant ${merchant.id}`);
-      return await fetchWebstiteData(merchant.websiteUrl);
-    });
+    const isGstNumberVerified = await step.run(
+      "verify-gst-number",
+      async () => {
+        console.log(`Verifying GST for merchant ${merchant.id}`);
+
+        return gstNumberVerification(merchant.gstNumber);
+      },
+    );
+
+    const { websiteData, isWebsiteVerified } = await step.run(
+      "verify-website",
+      async () => {
+        console.log(
+          `Verifying website and getting data for merchant ${merchant.id}`,
+        );
+
+        return fetchWebstiteData(merchant.websiteUrl);
+      },
+    );
 
     const mlPredictionData = await step.run("run-ml-prediction", async () => {
       console.log(`Running ML prediction for merchant ${merchant.id}`);
-      return await logRegPrediction(recentPayments, {isGstNumberVerified, isPhoneNumberVerified, isWebsiteVerified});
+
+      return logRegPrediction(recentPayments, {
+        isGstNumberVerified,
+        isPhoneNumberVerified,
+        isWebsiteVerified,
+      });
     });
+
+    const pipelineResults: PipelineResults = {
+      merchant,
+      verification,
+      recentPayments,
+      isPhoneNumberVerified,
+      isGstNumberVerified,
+      websiteData,
+      isWebsiteVerified,
+      mlPredictionData,
+    };
 
     const result = await step.run("combine-results", async () => {
-      return {
-        isPhoneNumberVerified,
-        isGstNumberVerified,
-        isWebsiteVerified,
-        mlPredictionData,
-        websiteData,
-      }
+      return combineResults(pipelineResults);
     });
 
-    return result;
+    const updatedVerificationData = await step.run(
+      "update-verification",
+      async () => {
+        console.log(
+          `Updating verification ${verification.id} for merchant ${merchant.id}`,
+        );
+
+        return updateVerification(pipelineResults, result);
+      },
+    );
+
+    console.log(
+      `Updated Verification: ${verification.id} for merchant: ${merchant.id}`,
+    );
+
+    return updatedVerificationData;
   },
 );
