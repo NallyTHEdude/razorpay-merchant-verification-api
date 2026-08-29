d5dbc95155af459acfd3456af9405b251d9adeab
Nawaz <rifaqatnawaz25@gmail.com>
2026-08-30 00:11:57 +0530

    feat: added types for pipeline data

---
+++ src/data/types/pipelineTypes.d.ts
@@ -0,0 +1,50 @@
+import { Merchant } from "@/data/types/Merchant";
+import { Verification } from "@/data/types/Verification";
+import { Payment } from "@/data/types/Payment";
+import { RiskLevel, VerificationStatus } from "@/data/enums/db.enums";
+
+export type VerificationResults = {
+  isPhoneNumberVerified: boolean;
+  isGstNumberVerified: boolean;
+  isWebsiteVerified: boolean;
+};
+
+export type MLPredictionData = {
+  paymentCount: number;
+  averagePaymentAmount: number;
+  failedPaymentRate: number;
+  highValuePaymentRate: number;
+  internationalPaymentRate: number;
+
+  isGstNumberVerified: boolean;
+  isPhoneNumberVerified: boolean;
+  isWebsiteVerified: boolean;
+
+  fraudProbability: number;
+  riskLevel: "LOW" | "MEDIUM" | "HIGH";
+};
+
+export type WebsiteData = unknown;
+
+export type PipelineResults = {
+  merchant: Merchant;
+  verification: Verification;
+  recentPayments: Payment[];
+
+  isPhoneNumberVerified: boolean;
+  isGstNumberVerified: boolean;
+
+  websiteData: WebsiteData;
+  isWebsiteVerified: boolean;
+
+  mlPredictionData: MLPredictionData;
+};
+
+export type VerificationUpdateData = {
+  verificationStatus: VerificationStatus;
+  isPhoneNumberVerified: boolean;
+  isGstNumberVerified: boolean;
+  isWebsiteVerified: boolean;
+  trustscore: number;
+  riskLevel: RiskLevel;
+};
