07254a854edf3e5903d3197d9167af08bbf84996
Nawaz <rifaqatnawaz25@gmail.com>
2026-08-29 23:37:55 +0530

    chore: better ml_service_url validation

--- src/config/env/env.ts
+++ src/config/env/env.ts
@@ -11,7 +11,7 @@ const envSchema = z.object({
   BASE_URL: z.string(),
   PORT: z.coerce.number().int().min(0).max(65535).default(3000),
   DATABASE_URL: z.string().url(),
-  ML_SERVICE_URL: z.string().url()
+  ML_SERVICE_URL: z.string().url(),
 });
 
 export const config = envSchema.parse(process.env);
