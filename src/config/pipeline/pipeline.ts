31216b1cb7c395a94c9612e8fbb62df7b5aab2ce
Nawaz <rifaqatnawaz25@gmail.com>
2026-08-31 11:41:12 +0530

    chore: removed duplicate imports

--- src/config/pipeline/pipeline.ts
+++ src/config/pipeline/pipeline.ts
@@ -1,5 +1,5 @@
 import { type Merchant } from "@/data/types/Merchant";
-import { type PipelineResults } from "@/data/types/pipelineTypes";
+import type{ PipelineResults, PipelinePayment } from "@/data/types/pipelineTypes";
 
 import { getByVerificationId } from "@/app/repositories/verification.repository";
 
@@ -12,7 +12,6 @@ import { logRegPrediction } from "@/app/verification-pipeline-stages/ml-predicti
 import { combineResults } from "@/app/verification-pipeline-stages/combine-results/combine-results";
 import { updateVerification } from "@/app/verification-pipeline-stages/update-verification/update-verification";
 import { updateMerchant } from "@/app/repositories/merchant.repository";
-import { type PipelinePayment } from "@/data/types/pipelineTypes";
 
 // Load existing verification and recent payments
 export const loadVerificationContext = async (
