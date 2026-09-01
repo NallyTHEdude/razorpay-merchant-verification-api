b4ea37dbdde4b8ce423b880ce1b710ba04a9733f
Nawaz <rifaqatnawaz25@gmail.com>
2026-09-01 08:57:17 +0530

    chore: updated documentUploadOptions, and uploadMerchantDto

--- src/data/types/Document.d.ts
+++ src/data/types/Document.d.ts
@@ -2,4 +2,10 @@ import type { CloudinaryFolderName } from "@/data/enums/cloudinary.enums";
 
 export type DocumentUploadOptions = {
   folder: CloudinaryFolderName;
+  subFolder: string;
+};
+
+export type UploadMerchantDto = {
+  fileBuffer: Buffer;
+  merchantId: string;
 };
